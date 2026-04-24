a54447ed7d354566850feb00cbc480dd6610035a
wes10566 <154547446+wes10566@users.noreply.github.com>
2026-04-24 11:36:57 -0500

    Editing spectral output part 1

--- code/SpectralClustering.py
+++ code/SpectralClustering.py
@@ -20,11 +20,6 @@ def spectralClustering(graph: nx.Graph, k: int):
     eigVal, eigVec = np.eigh(laplacianMatrix)
     eigVec = eigVec[:, 0:k]
 
-    print("\n\n\n")
-    print("New Eigens")
-    print(eigVec)
-
-
     row_norms = np.norm(eigVec, axis=1, keepdims=True)
     
     # Avoid division by zero
@@ -38,23 +33,10 @@ def spectralClustering(graph: nx.Graph, k: int):
     print("\n\nLabels\n")
     print(labelsNorm)
 
-    labelsStrange = kmeans.fit_predict(eigVec)
-    print("\n\nLabels Strange\n")
-    print(labelsStrange)
-
-
     for label in labelsNorm:
         nodeLabel = "Node " + str(labelNum) + " : Group " + str(labelsNorm[labelNum])
         nodesOutputArray.append(nodeLabel)
         labelNum += 1
-
-    #Below is for testing
-    labelNum = 0
-    for label in labelsStrange:
-        nodeLabel = "Node " + str(labelNum) + " : Group " + str(labelsStrange[labelNum])
-        strangeArray.append(nodeLabel)
-        labelNum += 1
-    
 
     print(nodesOutputArray)
     return nodesOutputArray
